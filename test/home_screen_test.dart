23b66c7645c823e60b60eff352173ec7284ac98f
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-20 19:08:13 +0330

    test: update home test for redesigned dashboard

--- test/home_screen_test.dart
+++ test/home_screen_test.dart
@@ -3,18 +3,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fcbaz/features/home/home_screen.dart';
 
 void main() {
-  testWidgets('FCBaz home renders Persian content', (tester) async {
+  testWidgets('FCBaz home renders Persian dashboard', (tester) async {
     await tester.pumpWidget(
       MaterialApp(
-        home: HomeScreen(onOpenPlayers: () {}),
+        home: HomeScreen(
+          onOpenPlayers: () {},
+          onOpenSearch: () {},
+          onOpenSquad: () {},
+          onOpenMore: () {},
+        ),
       ),
     );
 
-    expect(find.text('همه چیز درباره FC27'), findsOneWidget);
-
-    await tester.drag(find.byType(ListView), const Offset(0, -320));
-    await tester.pumpAndSettle();
-
+    expect(find.text('FCBaz'), findsOneWidget);
     expect(find.text('دسترسی سریع'), findsOneWidget);
+    expect(find.text('بازیکنان'), findsOneWidget);
+    expect(find.text('تیم‌ساز'), findsOneWidget);
   });
 }
