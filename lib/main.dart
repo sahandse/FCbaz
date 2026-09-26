5ff1f989fde32ab948b7a98d55d34b269960a6e0
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-26 14:09:01 +0330

    Schedule background price checks on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'features/notifications/background_price_worker.dart';
 import 'features/notifications/system_notification_service.dart';
 import 'src/fcbaz_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemNotificationService.instance.initialize();
+  await BackgroundPriceWorker.initialize();
+  await BackgroundPriceWorker.ensureScheduled();
   runApp(const FCBazApp());
 }
